d5a414bf85c6a236d1388a7304d9385d5c0f66a8
Rupak Das <dasr16983@gmail.com>
2024-07-02 08:48:42 +0530

    Some designs added

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,14 @@ function App() {
 
   return (
     <>
-      <h1> Digital Clock Made By Rupak Das</h1>
+      <div className='head-center'>
+        <h1> Digital Clock Made By Rupak Das</h1>
+      </div>
       <div className='outer'>
         <div className='inner'>
-          <div className='hour'>&nbsp;{hour <10  ? '0' +  hour : hour }&nbsp;</div>&nbsp;: &nbsp;
-          <div className='minute'>&nbsp;{minute <10  ? '0' + minute : minute }&nbsp;</div>&nbsp;: &nbsp;
-          <div className='second'>&nbsp;{ second <10 ? '0' + second : second }&nbsp;</div>
+          <div className='hour'>&nbsp;{hour < 10 ? '0' + hour : hour}&nbsp;</div>&nbsp;: &nbsp;
+          <div className='minute'>&nbsp;{minute < 10 ? '0' + minute : minute}&nbsp;</div>&nbsp;: &nbsp;
+          <div className='second'>&nbsp;{second < 10 ? '0' + second : second}&nbsp;</div>
         </div>
       </div>
     </>
